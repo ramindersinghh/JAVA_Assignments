32a61edb2fc124320d7787bdec46a980d2a549af
ramindersinghh <raminder.singh@zemosolabs.com>
2021-06-24 15:43:45 +0530

    Initial Commit

---
+++ src/com/assignment2/main/Driver.java
@@ -0,0 +1,24 @@
+package com.assignment2.main;
+
+import com.assignment2.utility.CheckAlphabets;
+import com.assignment2.utility.SimpleJavaStream;
+
+import java.util.Scanner;
+
+public class Driver {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        CheckAlphabets checkAlphabets=new CheckAlphabets();
+        System.out.println("Enter a String input:");
+        if (checkAlphabets.verifyAllLetters(scanner.next()))
+            System.out.println("String contains all alphabets");
+        else
+            System.out.println("String DOESN'T contains all alphabets");
+
+        SimpleJavaStream.operations();
+    }
+}
+/*
+abcdefghijklmnopqrstuvwxyz
+abcdefghijklmnopqrstuvwxyZ
+ */
